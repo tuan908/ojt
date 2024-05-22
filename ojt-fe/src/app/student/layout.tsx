61c4e123d8eca92906fd109f524579d76710bf1c
tuanna <tuan.a1.bt@gmail.com>
2024-05-22 20:44:27 +0700

    fix(build): cant find navbar

--- ojt-fe/src/app/student/layout.tsx
+++ ojt-fe/src/app/student/layout.tsx
@@ -1,4 +1,4 @@
-import Navbar from "@/components/Navbar";
+import Navbar from "@/components/navbar";
 import type {LayoutProps} from "@/types";
 
 export default function Layout({children}: LayoutProps) {
