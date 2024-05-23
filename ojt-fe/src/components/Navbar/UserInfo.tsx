f604e57201a15b0a7a1635ea641c0274cbbec157
Nguyen Anh Tuan <tuanna@ait.com>
2024-05-23 13:18:37 +0700

    update pg

---
+++ ojt-fe/src/components/Navbar/UserInfo.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import type {OjtJwtPayload} from "@/lib/auth";
+import {useAuth} from "@/lib/hooks/useAuth";
+import {convertRole} from "@/lib/utils/role";
+import {useEffect, useState} from "react";
+
+type UserPayload = Partial<Pick<OjtJwtPayload, "name" | "role">>;
+
+// TODO: Remove flicker when set user info
+export default function UserInfo() {
+    const {auth} = useAuth();
+    const [info, setInfo] = useState<UserPayload | null>(null);
+
+    useEffect(() => {
+        if (auth) {
+            setInfo({
+                name: auth.name,
+                role: convertRole(auth.role),
+            });
+        }
+    }, [auth]);
+
+    return (
+        <div className="flex flex-col">
+            <h1 className="text-xl font-normal text-[#abb7bc]">
+                こにちは {info ? info.name : null}
+            </h1>
+            <h1 className="text-[#c3cbcf]">{info ? info.role : null}</h1>
+        </div>
+    );
+}
